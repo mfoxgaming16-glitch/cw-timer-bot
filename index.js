ff3808d49b1a2a100241c15174bedc395ba433fc
mfoxgaming16-glitch <mfoxgaming16@gmail.com>
2026-04-12 13:06:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ const {
 const { DateTime } = require("luxon");
 const fs = require("fs");
 const path = require("path");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive");
+});
+
+app.listen(3000, () => {
+  console.log("Web server running on port 3000");
+});
 
 const TOKEN = process.env.DISCORD_TOKEN;
 const CHANNEL_ID = process.env.CHANNEL_ID;
